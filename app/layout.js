d1e8a841b7875cae58614b281a32ccd4dba439b9
Gagan-Narikadamalli <b24g23@iastate.edu>
2026-09-21 09:52:46 -0400

    Add app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './style.css';
+export const metadata={title:'Success On The Spectrum | Call-Out & Time-Off Portal',description:'Call-out and time-off reporting portal'};
+export default function Layout({children}){return <html lang="en"><body>{children}</body></html>}
